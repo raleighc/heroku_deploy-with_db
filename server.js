516d17167a6a92abbc24774b87fc44b6ceaff2c3
Raleigh Chesney <whileiwasthinking@yahoo.com>
2020-10-08 09:45:42 -0400

    created handlebars and connected

--- server.js
+++ server.js
@@ -6,11 +6,28 @@ const app = express();
 
 const PORT = process.env.PORT || 8080;
 
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+// app.use(express.static("public"));
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+
+// VIEWS ROUTES
+app.get("/", (req, res) => {
+    res.render("index");
+})
+
+
+// API ROUTES
+
 app.get("/api/config", (req, res) => {
   res.json({
     success: true,
   });
 });
+
 
 app.listen(PORT, function () {
   console.log("Server listening on: http://localhost:" + PORT);
